c40a5fed8753218a97bad00e37b4fc0a0b6de7a9
Quang Vinh <51204518@hcmut.edu.vn>
2016-03-15 11:23:27 +0700

    - Add hover effect for membercard ( Winston effect ) - Change icons of 3 buttons : edit , add , del - Add titles for 3 buttons above via JS

--- js/tree.js
+++ js/tree.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-  var leaf = $(".membercard").size();
-  $(".tree").width(leaf*270);
-})
+    var leaf = $(".membercard").size();
+    $(".tree").width(leaf*270);
+
+    $(".edit-icon-title").attr("title", "Edit");
+    $(".add-icon-title").attr("title", "Add relative");
+    $(".delete-icon-title").attr("title", "Delete");
+});
