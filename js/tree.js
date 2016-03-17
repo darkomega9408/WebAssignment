c1d9ca6a3bc190547501c51af665ba696f20635e
Quang Vinh <51204518@hcmut.edu.vn>
2016-03-17 17:15:10 +0700

    - YOU MUST USE tree-origin/tree.html - Custom tree & member card - Add hover icon : Edit , Add Relative , Delete effect to member-card.css

--- js/tree.js
+++ js/tree.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-    var leaf = $(".membercard").size();
-    $(".tree").width(leaf*270);
 
-    $(".edit-icon-title").attr("title", "Edit");
-    $(".add-icon-title").attr("title", "Add relative");
-    $(".delete-icon-title").attr("title", "Delete");
-});
