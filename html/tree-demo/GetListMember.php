81b35fc72ad5637afa4d6579e810f00d0d091315
unknown <nxthai2910@gmail.com>
2016-03-20 00:52:48 +0700

    basic login flow

--- html/tree-demo/GetListMember.php
+++ html/tree-demo/GetListMember.php
@@ -2,7 +2,7 @@
 try {
     $conn = new PDO("mysql:host=localhost;dbname=webassignment", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT MemberID, Name, BirthDate, Address, BirthPlace, Gender, Father, Level, Avatar FROM `member` WHERE Username = 'tam'");
+    $stmt = $conn->prepare("SELECT MemberID, Name, BirthDate, Address, BirthPlace, Gender, Father, Level, Avatar FROM `member` WHERE userID = 2");
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     echo json_encode($stmt->fetchAll());
