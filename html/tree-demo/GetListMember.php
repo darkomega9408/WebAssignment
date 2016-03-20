9c738ae4651f98396babb90acc1e1b859f489af9
Qui <zv0ng0laprim0z@gmail.com>
2016-03-20 17:27:13 +0700

    add selectize.js

--- html/tree-demo/GetListMember.php
+++ html/tree-demo/GetListMember.php
@@ -1,8 +1,8 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=webassignment", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $conn = new PDO("mysql:host=localhost;dbname=family-tree", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT MemberID, Name, BirthDate, Address, BirthPlace, Gender, Father, Level, Avatar FROM `member` WHERE Username = 'tam'");
+    $stmt = $conn->prepare("SELECT MemberID, Name, BirthDate, Address, BirthPlace, Gender, Father, Level, Avatar FROM `member` WHERE userID = 2");
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     echo json_encode($stmt->fetchAll());
